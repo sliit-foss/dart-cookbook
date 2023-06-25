8c3ada77b0dcf4aa2e343a7b4665bb110eb91643
Akalanka <akalankaperera128@gmail.com>
2023-06-26 01:20:34 +0530

    Feat: array/compact generic support

--- packages/flodash/lib/modules/array/chunk.dart
+++ packages/flodash/lib/modules/array/chunk.dart
@@ -1,11 +1,13 @@
 /// Creates an array of elements split into groups the length of size. If array can't be split evenly, the final chunk will be the remaining elements.
 ///
 /// Arguments
-/// list (List): The array to process.
-/// [size=1] (int): The length of each chunk
+///
+/// - list (List): The array to process.
+/// - [size=1] (int): The length of each chunk
 ///
 /// Returns
-/// (List): Returns the new array of chunks.
+///
+/// - (List): Returns the new array of chunks.
 
 List<List<T>> chunk<T>(List<T> list, {int size = 1}) {
   List<List<T>> chunks = [];
--- packages/flodash/lib/modules/array/compact.dart
+++ packages/flodash/lib/modules/array/compact.dart
@@ -1,5 +1,15 @@
 import '../lang/main.dart';
 
-List compact(List list) {
+/// Creates an array with all falsey values removed. The values false, null, 0, "", undefined, and NaN are falsey.
+///
+/// Arguments
+///
+/// - list (List): The array to compact.
+///
+/// Returns
+///
+/// - (List): Returns the new array of filtered values.
+
+List<T> compact<T>(List<T> list) {
   return list.where((e) => !isFalsy(e)).toList();
 }
