cb15a60d0da6eb72652e28e7eb9e3f91ae7f6c38
Akalanka <akalankaperera128@gmail.com>
2023-06-26 00:23:01 +0530

    Feat: added generics

--- packages/flodash/lib/modules/array/chunk.dart
+++ packages/flodash/lib/modules/array/chunk.dart
@@ -1,9 +1,18 @@
-List chunk(List list, {int size = 1}) {
-  List chunks = [];
+/// Creates an array of elements split into groups the length of size. If array can't be split evenly, the final chunk will be the remaining elements.
+///
+/// Arguments
+/// list (List): The array to process.
+/// [size=1] (int): The length of each chunk
+///
+/// Returns
+/// (List): Returns the new array of chunks.
+
+List<List<T>> chunk<T>(List<T> list, {int size = 1}) {
+  List<List<T>> chunks = [];
   for (int i = 0; i < list.length; i += size) {
     int end = i + size;
     if (end > list.length) end = list.length;
-    chunks.add(list.sublist(i, end));
+    chunks.add(list.sublist(i, end).cast<T>());
   }
   return chunks;
 }
