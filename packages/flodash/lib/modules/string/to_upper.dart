4218404b6c336ddcdc40ad812304b3ddcf83f451
GIHAA <gihansudeeparandimal@gmail.com>
2023-06-30 19:19:38 +0530

    Refactor: changes added

--- packages/flodash/lib/modules/string/to_upper.dart
+++ packages/flodash/lib/modules/string/to_upper.dart
@@ -1,4 +1,15 @@
-///Converts string, as a whole, to upper cas
+/// Converts a string, as a whole, to uppercase.
+///
+/// Arguments
+/// 
+/// - (string): The string to convert.
+///
+/// Returns
+/// 
+/// - (string): The uppercase string.
+
+
+@Deprecated('The function toUpper() is deprecated. Use the built-in toUpperCase() method instead.')
 String toUpper([String string = '']) {
   return string.toUpperCase();
 }
--- packages/flodash/test/modules/string/to_upper_test.dart
+++ packages/flodash/test/modules/string/to_upper_test.dart
@@ -2,25 +2,25 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flodash/flodash.dart' as flodash;
 
 void main() {
-  test('toUpper should convert the string to upper case', () {
+  test('should convert the string to upper case', () {
     String input = '--foo-bar--';
     String expected = '--FOO-BAR--';
     String result = flodash.toUpper(input);
     expect(result, expected);
   });
-  test('toUpper should convert the string to upper case when it contains both lowercase and uppercase characters', () {
+  test('should convert the string to upper case when it contains both lowercase and uppercase characters', () {
     String input = 'fooBar';
     String expected = 'FOOBAR';
     String result = flodash.toUpper(input);
     expect(result, expected);
   });
-  test('toUpper should convert the string to upper case while preserving special characters', () {
+  test('should convert the string to upper case while preserving special characters', () {
     String input = '__foo_bar__';
     String expected = '__FOO_BAR__';
     String result = flodash.toUpper(input);
     expect(result, expected);
   });
-  test('toUpper should return an empty string if no input is provided', () {
+  test('should return an empty string if no input is provided', () {
     String input = '';
     String expected = '';
     String result = flodash.toUpper(input);
