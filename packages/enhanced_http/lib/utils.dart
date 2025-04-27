e40e4cf40ef1279fae63423444761043377004b0
Akalanka Perera <akalankaperera128@gmail.com>
2025-04-27 13:59:51 +0530

    Style: formatted code

--- packages/enhanced_http/lib/utils.dart
+++ packages/enhanced_http/lib/utils.dart
@@ -1,8 +1,13 @@
 class Utils {
-  Map<String, String> mergeHeaders(Map<String, String>? h1, Map<String, String>? h2, [Map<String, String>? h3]) => {...?h1, ...?h2, ...?h3};
+  Map<String, String> mergeHeaders(
+          Map<String, String>? h1, Map<String, String>? h2,
+          [Map<String, String>? h3]) =>
+      {...?h1, ...?h2, ...?h3};
 
   bool isStream(Map<String, String>? headers, [String? responseType = "json"]) {
-    if ([headers?["Content-Type"], headers?["content-type"]].contains("multipart/form-data") || responseType == "stream") {
+    if ([headers?["Content-Type"], headers?["content-type"]]
+            .contains("multipart/form-data") ||
+        responseType == "stream") {
       return true;
     }
     return false;
